42f6396fa5efb3c6cb24873fcf75b2cf800106d3
Paulo Salvatore <salvatoregames@gmail.com>
2023-05-29 19:24:12 -0300

    chore(prettier): install package

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
-import Section1 from './components/sections/Section1';
+import "./App.css";
+import Section1 from "./components/sections/Section1";
 
 export default function App() {
   return (
     <>
-      <Section1/>
+      <Section1 />
     </>
-  )
+  );
 }
--- src/components/sections/Section1.tsx
+++ src/components/sections/Section1.tsx
@@ -1,11 +1,9 @@
-import './Section1.css'
+import "./Section1.css";
 
 export default function Section1() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Section1
-      </h1>
+      <h1 className="text-3xl font-bold underline">Section1</h1>
     </>
-  )
+  );
 }
